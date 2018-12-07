ebb1a136ab31141826b83b747ae2c14918eb15fc
Andrii <andrey.grisch@gmail.com>
2018-12-07 15:44:18 +0000

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+import './styles/main.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <header className="we-header"></header>
+                <div>test</div>
+                <footer className="we-footer"></footer>
+            </Fragment>
+        );
+      }
+};
---
+++ src/reducers/EventReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+};
+
+export default function events(state = initialState, action) {
+    switch (action.type) {
+        case '':
+            return state
+
+        default:
+            return state
+    }
+}
